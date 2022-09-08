19ea4a3c87bda629ef94556202412c0892ef2c6b
Maksym Voron <maksym.voron@sensoneo.com>
2022-09-08 18:38:54 +0200

    react-redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React from 'react'
+import {useState} from 'react'
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todos,setTodos] = useState([]);
+  const [text, setText] = useState('')
+
+  const addTodo = ()=> {
+    if(text.trim().length){
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed:false,
+        }
+      ])
+      setText('');
+    }
+  }
+
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId))
+  }
+
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map(
+        todo =>
+        {
+          if (todo.id !== todoId) return todo;
+
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <label>
+          <input value={text} onChange={(e) => setText(e.target.value)} />
+          <button onClick={addTodo} className='btn btn-outline-primary mx-3 p-1'>Add Todo</button>
+        </label>
+
+        <ul>
+          {
+            todos.map(todo =>
+            <li key={todo.id}>
+                <input type='checkbox'  checked={todo.completed} onChange={() => toggleTodoComplete(todo.id)}/>
+                <span>{todo.text}</span>
+                <span className='text-danger' role='button' onClick={() => removeTodo(todo.id)}>&times;</span>
+            </li> )
+          }
+        </ul>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
